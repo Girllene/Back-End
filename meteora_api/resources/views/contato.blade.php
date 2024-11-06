ffe69e8674dd0af052ec48dfe9e3d016abec8289
Seu Nome <seu_email@example.com>
2024-11-05 21:04:11 -0300

    aula_13

---
+++ meteora_api/resources/views/contato.blade.php
@@ -0,0 +1,12 @@
+<h1>Aqui irá ficar a página contato</h1>
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/">Sobre</a>
+    </li>
+    <li>
+        <a href="/">Contato</a>
+    </li>
+</ul>
---
+++ meteora_api/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<h1>Aqui vai ficar a página inicial!</h1>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/">Sobre</a>
+    </li>
+    <li>
+        <a href="/">Contato</a>
+    </li>
+</ul>
